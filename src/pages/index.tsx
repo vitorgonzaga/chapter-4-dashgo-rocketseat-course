030c73eafbf39a3108c9b98b61c372ebda1cc160
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-06 10:51:52 -0300

    chore: add SignIn page and development of an customized Input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+import { Button, Flex, VStack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <h1>dashgo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        p="8"
+        borderRadius={8}
+        bg='gray.800'
+        direction="column"
+      >
+        <VStack spacing="4" >
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </VStack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
