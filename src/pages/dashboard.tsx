f7a0c18b3f6eac24a3649ae783510682caa25680
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-06 15:21:27 -0300

    chore(dashboard): initializing development of charts in dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,33 @@
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" h="100vh">
      <Header />
-    </>
+     <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+      <Sidebar />
+      <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+        <Box
+          p="8"
+          bg="gray.800"
+          borderRadius={8}
+          // pb="4"
+        >
+          <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+        </Box>
+        <Box
+          p="8"
+          bg="gray.800"
+          borderRadius={8}
+          // pb="4"
+        >
+          <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+        </Box>
+
+      </SimpleGrid>
+     </Flex>
+    </Flex>
   )
 }
