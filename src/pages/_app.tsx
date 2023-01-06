2ff35e8849efd309ade27892217afce9e0cfdf33
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-06 09:15:20 -0300

    chore: sets some theme customization in ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,39 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#181B23",
+      "800": "#1F2029",
+      "700": "#353646",
+      "600": "#4B4D63",
+      "500": "#616480",
+      "400": "#797D9A",
+      "300": "#9699B0",
+      "200": "#B3B5C6",
+      "100": "#D1D2DC",
+      "50": "#EEEEF2"
+    }
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50'
+      }
+    }
+  }
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
