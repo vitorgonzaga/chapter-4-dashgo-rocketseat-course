8cde5152c512981c554ca8662cffab6ab81d97b8
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-08 08:08:09 -0300

    chore: working on Header responsivity

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,22 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
-  return (
+interface ProfileProps {
+  showProfileData?: boolean
+}
 
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
     <Flex align="center">
-      <Box mr="4"textAlign="right">
-        <Text>Vitor Ferreira</Text>
-        <Text color="gray.300" fontSize="small">
-          vitor.gonzaga@gmail.com
-        </Text>
-      </Box>
+      {
+        showProfileData && (
+          <Box mr="4"textAlign="right">
+            <Text>Vitor Ferreira</Text>
+            <Text color="gray.300" fontSize="small">
+              vitor.gonzaga@gmail.com
+            </Text>
+          </Box>
+        )
+      }
 
       <Avatar size="md" name="Vitor Ferreira" src="https://github.com/vitorgonzaga.png"/>
 
