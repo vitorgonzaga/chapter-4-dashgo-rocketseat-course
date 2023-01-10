f6ea691d0e1e93220663c57468c52adf25b5c21b
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-10 08:02:24 -0300

    chore: working with react-hook-form and yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
 import { Button, Flex, VStack } from "@chakra-ui/react";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import * as yup from 'yup';
 import { Input } from "../components/Form/Input";
 
+// type SignInFormData = {
+//   email: FieldValues;
+//   password: FieldValues;
+// }
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const { errors } = formState
+
+  const handleSignIn: SubmitHandler<FieldValues> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(values)
+  }
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,13 +36,32 @@ export default function SignIn() {
         borderRadius={8}
         bg='gray.800'
         direction="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <VStack spacing="4" >
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            type="email"
+            label="E-mail"
+            error={errors.email?.message}
+            { ...register("email") }
+          />
+          <Input
+            type="password"
+            label="Senha"
+            error={errors.password?.message}
+            {...register("password")}
+          />
         </VStack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,36 +1,98 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import { yupResolver } from '@hookform/resolvers/yup';
 import Link from "next/link";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import * as yup from 'yup';
+
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória').min(6, 'Mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([
+    null,
+    yup.ref('password')
+  ], 'As senhas precisam ser iguais')
+})
+
 export default function CreateUser() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema)
+  })
+
+  const { errors } = formState
+
+  const handleCreateUser: SubmitHandler<FieldValues> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(values)
+  }
+
   return (
     <Box>
       <Header />
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["6","8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
           <Divider my="6" borderColor="gray.700" />
           <VStack spacing="8" >
             <SimpleGrid minChildWidth={240} spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome Completo" />
-              <Input name="email" type="email" label="E-mail" />
+              <Input
+                // name="name"
+                label="Nome Completo"
+                error={errors.name?.message}
+                {...register('name')}
+              />
+              <Input
+                // name="email"
+                type="email"
+                label="E-mail"
+                error={errors.email?.message}
+                {...register('email')}
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth={240} spacing={["6", "8"]} w="100%">
-              <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmação de senha" />
+              <Input
+                // name="password"
+                type="password"
+                label="Senha"
+                error={errors.password?.message}
+                {...register('password')}
+              />
+              <Input
+                // name="password_confirmation"
+                type="password"
+                label="Confirmação de senha"
+                error={errors.password_confirmation?.message}
+                {...register('password_confirmation')}
+              />
             </SimpleGrid>
           </VStack>
 
           <Flex mt={["6", "8"]} justify="flex-end">
             <HStack spacing="4" >
               <Link href="/users" passHref>
-                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+                <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button
+                type="submit"
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
 
